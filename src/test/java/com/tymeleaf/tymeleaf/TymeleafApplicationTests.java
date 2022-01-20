6db4eba3015274cdc42db3a11c50afa09ba254f0
ljh468 <ljh468@mibank.me>
2022-01-20 18:16:58 +0900

    타임리프 기본세팅

---
+++ src/test/java/com/tymeleaf/tymeleaf/TymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tymeleaf.tymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
