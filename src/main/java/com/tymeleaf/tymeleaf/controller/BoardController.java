0c18b762ffea9f696af035bcbe66d239bded4221
ljh468 <ljh468@mibank.me>
2022-01-26 20:24:08 +0900

    th:href 게시판

--- src/main/java/com/tymeleaf/tymeleaf/controller/BoardController.java
+++ src/main/java/com/tymeleaf/tymeleaf/controller/BoardController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -30,4 +32,16 @@ public class BoardController {
         return "board/list";
     }
 
+    @GetMapping("/form")
+    public String form(Model model) {
+        model.addAttribute("board", new Board());
+        return "board/form";
+    }
+
+    @PostMapping("/form")
+    public String greetingSubmit(@ModelAttribute Board board){
+
+        Board save = boardRepository.save(board);
+        return "redirect:/board/list";
+    }
 }
